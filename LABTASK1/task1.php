055cce6a4826ff0a623dc76289b258b153bb58bf
Mahmud Polok <63794951+MahmudPolok@users.noreply.github.com>
2022-06-01 23:54:06 +0600

    Add files via upload

---
+++ LABTASK1/task1.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Donor Information</h2>
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"required value=""><br><br>
+ 
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname"required value=""><br><br>
+ 
+  <label for="Cname">Company:</label>
+  <input type="text" id="Cname" name="Cname"required value=""><br><br>
+ 
+  <label for="Aname1">Address 1:</label>
+  <input type="text" id="Aname1" name="Aname1"required value=""><br><br>
+ 
+  <label for="Aname2">Address 2:</label>
+  <input type="text" id="Aname2" name="Aname2"required value=""><br><br>
+ 
+  <label for="Cname">City:</label>
+  <input type="text" id="Cname" name="Cname"required value=""><br><br>
+ 
+  <label for="Sname">State:</label>
+  <select id="Sname" name="Sname" required>
+  <option value="Select a State">Select a State</option>
+  </select><br><br>
+ 
+  <label for="lname">Zip Code:</label>
+  <input type="text" id="lname" name="lname"required value=""><br><br>
+  
+  <label for="Coname">Country:</label>
+  <select id="Sname" name="Sname"required>
+  <option value="Select a Country">Select a Country</option>
+  </select><br><br>
+  
+   <label for="lname">Phone:</label>
+  <input type="text" id="lname" name="lname"required value=""><br><br>
+  
+  <label for="lname">Fax:</label>
+  <input type="text" id="lname" name="lname"required value=""><br><br>
+  
+  <label for="lname">Email:</label>
+  <input type="text" id="lname" name="lname"required value=""><br><br>
+  
+  <label for="lname">Donation Amount:</label>
+  <input type="radio" id="lname" name="lname"required value="">None
+  <input type="radio" id="lname" name="lname"required value="">50$
+  <input type="radio" id="lname" name="lname"required value="">100$
+  <input type="radio" id="lname" name="lname"required value="">150$
+    <input type="radio" id="lname" name="lname"required value="">200$
+  <input type="radio" id="lname" name="lname"required value="">250$
+    <input type="radio" id="lname" name="lname"required value="">Other<br><br>
+ 
+  <label for="lname">Other Amount $:</label>
+  <input type="text" id="lname" name="lname" value=""><br><br>
+  
+    <label for="lname">Recouring Donation:</label>
+  <input type="checkbox" id="lname" name="lname" value=""> I am interrested in giving on a regular basis<br><br>
+  
+   <label for="lname">Monthly Credit Card:</label>
+  <input type="number" id="lname" name="lname" value=""> for
+  <input type="month" id="lname" name="lname" value="">month<br><br>
+  
+  <h2>Honorarium and Memorial Donation Information</h2><br>
+  
+  <label for="lname">I would like to make this Donation:</label>
+  <input type="radio" id="lname" name="lname" value=""> To Honor<br>
+  <input type="radio" id="lname" name="lname" value=""> In Memeory of<br><br>
+  
+   <label for="Cname">Name:</label>
+  <input type="text" id="Cname" name="Cname"required value=""><br><br>
+ 
+  <label for="Aname1">Acknowledge Donation To:</label>
+  <input type="text" id="Aname1" name="Aname1"required value=""><br><br>
+ 
+  <label for="Aname2">Address:</label>
+  <input type="text" id="Aname2" name="Aname2"required value=""><br><br>
+ 
+  <label for="Cname">City:</label>
+  <input type="text" id="Cname" name="Cname"required value=""><br><br>
+ 
+  <label for="Sname">State:</label>
+  <select id="Sname" name="Sname" required>
+  <option value="Select a State">Select a State</option>
+  </select><br><br>
+ 
+  <label for="lname">Zip Code:</label>
+  <input type="text" id="lname" name="lname"required value=""><br><br>
+  
+  <h2>Additional Information</h2><br>
+  <p>Please enter your name, company or organization as you would like to appear in our publications</p><br>
+  
+  <label for="Cname">Name:</label>
+  <input type="text" id="Cname" name="Cname"required value=""><br><br>
+  
+  <input type="checkbox" id="lname" name="lname" value=""> I would like my gift to anonymous<br>
+  <input type="checkbox" id="lname" name="lname" value=""> My employer offers a machine gift program. I will mail the machine gift form<br>
+  <input type="checkbox" id="lname" name="lname" value=""> Please save the cost of acknowledging this gift by not mailing a thank you letter<br><br>
+  
+  <label for="Cname">Comments:</label>
+  <input type="" id="Cname" name="Cname"required value=""><br><br>
+  
+   <label for="lname"> How may we contact you:</label><br>
+  <input type="checkbox" id="lname" name="lname" value=""> E-mail<br>
+  <input type="checkbox" id="lname" name="lname" value=""> Postal Mail<br>
+  <input type="checkbox" id="lname" name="lname" value=""> Telephone<br>
+  <input type="checkbox" id="lname" name="lname" value=""> Fax<br>
+  <br>
+  
+  <input type="checkbox" id="lname" name="lname" value=""> I would like information about volunteering with the<br>
+  <br>
+  
+  <input type="reset" value="Reset">
+  <input type="button" value="Continue">
+</form>
+</body>
+</html>
